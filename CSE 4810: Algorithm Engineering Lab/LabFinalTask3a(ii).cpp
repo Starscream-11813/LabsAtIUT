c29dc617c1455979c779780c148aedca73bce1c5
Syed Rifat Raiyan <46789391+Starscream-11813@users.noreply.github.com>
2023-05-22 18:33:43 +0600

    Create LabFinalTask3a(ii).cpp

---
+++ CSE 4810: Algorithm Engineering Lab/LabFinalTask3a(ii).cpp
@@ -0,0 +1,147 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define LCM(a,b)                (a / __gcd(a,b) ) *b
+#define GCD(a,b)                __gcd(a,b)
+#define SQR(a)                  ((a)*(a))
+#define pb                      push_back
+#define mk                      make_pair
+#define ff                      first
+#define ss                      second
+#define all(a)                  a.begin(),a.end()
+#define lastEle(v)              v[v.size()-1]
+#define ABS(x)                  ((x)<0?-(x):(x))
+#define min3(a,b,c)             min(a,min(b,c))
+#define min4(a,b,c,d)           min(a,min(b,min(c,d)))
+#define max3(a,b,c)             max(a,max(b,c))
+#define max4(a,b,c,d)           max(a,max(b,max(c,d)))
+#define max5(a,b,c,d,e)         max(max3(a,b,c),max(d,e))
+#define min5(a,b,c,d,e)         min(min3(a,b,c),min(d,e))
+#define MEM(a,x)                memset(a,x,sizeof(a))
+#define Godspeed                ios_base::sync_with_stdio(0);cin.tie(NULL)
+#define urs(r...)               typename decay<decltype(r)>::type
+#define FOR(i,a,b)              for(urs(b) i=a;i<=b;i++)
+#define ROF(i,a,b)              for(urs(b) i=a;i>=b;i--)
+#define REP(i,b)                for(urs(b) i=0;i<b;i++)
+#define PER(i,a,b)              for(urs(b) i=a;i>b;i--)
+#define FOREACH(i,t)            for (__typeof(t.begin()) i=t.begin(); i!=t.end(); i++)
+#define PRESENT(c,x)            ((c).find(x) != (c).end())
+#define CPRESENT(c,x)           (find(all(c),x) != (c).end())
+#define SPRESENT(c,x)           ((c).find(x) != string::npos)
+#define Bye                     return 0
+#define ODD(x)                  (bool)(x&1)
+#define Debug                   cout<<endl<<"I AM BATMAN"<<endl;
+#define what_is(x)              cerr << __LINE__ <<": " << #x << " is " << x << endl;
+//Datatypes
+#define ll                      long long
+#define ULL                     unsigned long long
+#define LD                      long double
+#define UI                      unsigned int
+#define US                      unsigned short
+#define endl                    '\n'
+const long long int MOD = 1000000007; // 998244353
+const ll INF = 1e18;
+const ll inf = 0xFFFFFFFFFFFFFFFL;
+#define PI                      acos(-1.0)
+
+bool sortbysec(const pair<long long int,long long int> &a, const pair<long long int,long long int> &b)
+{
+    return (a.second < b.second);
+}
+
+//Debugging stuff
+#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+void err(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args)
+{
+    cerr << *it << " = " << a << endl;
+    err(++it, args...);
+}
+#define pii pair<int,int>
+#define pll pair<long long,long long>
+#define pdd pair<double,double>
+#define pldld pair<long double, long double>
+
+/*
+    Syed Rifat Raiyan
+    IUT CSE-18
+    Beautifully struggle every day~
+*/
+
+int main()
+{
+    Godspeed;
+    int Tests=1;
+    cin>>Tests;
+    while(Tests--)
+    {
+        ll n,Sum;
+        cin>>n>>Sum;
+        vector<ll>A(n);
+        REP(i,n)
+        {
+            cin>>A[i];
+        }
+        vector<vector<ll>>dp(n+1,vector<ll>(Sum+1,0LL));
+        REP(i,n+1)
+        {
+            dp[i][0]=1;
+        }
+        FOR(i,1,n)
+        {
+            FOR(j,1,Sum)
+            {
+                if(j<A[i-1])
+                {
+                    if(i>1)
+                    {
+                        dp[i][j]=dp[i-1][j];
+                    }
+                }
+                else
+                {
+                    if(i>1)
+                    {
+                        dp[i][j]=dp[i][j-A[i-1]]+dp[i-1][j];
+                    }
+                    else
+                    {
+                        dp[i][j]=dp[i][j-A[i-1]];
+                    }
+                }
+            }
+        }
+        REP(i,dp.size())
+        {
+            REP(j,dp[0].size())
+            {
+                cout<<dp[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        cout<<dp[n][Sum]<<endl;
+    }
+    Bye;
+}
+/*
+7
+4 9
+1 2 3 4
+3 4
+1 2 3
+8 252
+2 5 10 50 100 200 501 997
+9 37
+1 2 5 10 50 100 200 500 1000
+9 72
+1 2 5 10 50 100 200 500 1000
+9 101
+1 2 5 10 50 100 200 500 1000
+9 7
+1 2 5 10 50 100 200 500 1000
+*/
+
+
+
+
