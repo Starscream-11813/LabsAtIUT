1acec1c513d53af0c5e15d92f320a14114ad8ba6
Syed Rifat Raiyan <46789391+Starscream-11813@users.noreply.github.com>
2023-05-22 18:28:24 +0600

    Create Lab5Task1.cpp

---
+++ CSE 4810: Algorithm Engineering Lab/Lab5Task1.cpp
@@ -0,0 +1,149 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define LCM(a,b)                (a / __gcd(a,b) ) *b
+#define GCD(a,b)                __gcd(a,b)
+#define SQR(a)                  ((a)*(a))
+#define pb                      push_back
+#define mk                      make_pair
+#define ff                      first
+#define ss                      second
+#define all(a)                  a.begin(),a.end()
+#define lastEle(v)              v[v.size()-1]
+#define ABS(x)                  ((x)<0?-(x):(x))
+#define min3(a,b,c)             min(a,min(b,c))
+#define min4(a,b,c,d)           min(a,min(b,min(c,d)))
+#define max3(a,b,c)             max(a,max(b,c))
+#define max4(a,b,c,d)           max(a,max(b,max(c,d)))
+#define max5(a,b,c,d,e)         max(max3(a,b,c),max(d,e))
+#define min5(a,b,c,d,e)         min(min3(a,b,c),min(d,e))
+#define MEM(a,x)                memset(a,x,sizeof(a))
+#define Godspeed                ios_base::sync_with_stdio(0);cin.tie(NULL)
+#define urs(r...)               typename decay<decltype(r)>::type
+#define FOR(i,a,b)              for(urs(b) i=a;i<=b;i++)
+#define ROF(i,a,b)              for(urs(b) i=a;i>=b;i--)
+#define REP(i,b)                for(urs(b) i=0;i<b;i++)
+#define PER(i,a,b)              for(urs(b) i=a;i>b;i--)
+#define FOREACH(i,t)            for (__typeof(t.begin()) i=t.begin(); i!=t.end(); i++)
+#define PRESENT(c,x)            ((c).find(x) != (c).end())
+#define CPRESENT(c,x)           (find(all(c),x) != (c).end())
+#define SPRESENT(c,x)           ((c).find(x) != string::npos)
+#define Bye                     return 0
+#define ODD(x)                  (bool)(x&1)
+#define Debug                   cout<<endl<<"I AM BATMAN"<<endl;
+#define what_is(x)              cerr << __LINE__ <<": " << #x << " is " << x << endl;
+//Datatypes
+#define ll                      long long
+#define ULL                     unsigned long long
+#define LD                      long double
+#define UI                      unsigned int
+#define US                      unsigned short
+#define endl                    '\n'
+const long long int MOD = 1000000007; // 998244353
+const ll INF = 1e18;
+const ll inf = 0xFFFFFFFFFFFFFFFL;
+#define PI                      acos(-1.0)
+
+bool sortbysec(const pair<long long int,long long int> &a, const pair<long long int,long long int> &b)
+{
+    return (a.second < b.second);
+}
+
+//Debugging stuff
+#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+void err(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args)
+{
+    cerr << *it << " = " << a << endl;
+    err(++it, args...);
+}
+#define pii pair<int,int>
+#define pll pair<long long,long long>
+#define pdd pair<double,double>
+#define pldld pair<long double, long double>
+
+/*
+    Syed Rifat Raiyan
+    IUT CSE-18
+    Beautifully struggle every day~
+*/
+vector<vector<bool>>G,vis;
+vector<ll>dx={-1,0,1,0},dy={0,1,0,-1};
+ll res,land;
+ll m,n;
+
+void connectedComponents(ll i, ll j)
+{
+    if(G[i][j] && !vis[i][j])
+    {
+        vis[i][j]=true;
+        land++;
+        REP(k,4)
+        {
+            if((i+dx[k]>=0 && i+dx[k]<=m-1) && (j+dy[k]>=0 && j+dy[k]<=n-1))
+            {
+                connectedComponents(i+dx[k],j+dy[k]);
+            }
+        }
+    }
+}
+
+int main()
+{
+    Godspeed;
+    int Tests=1;
+    cin>>Tests;
+    while(Tests--)
+    {
+        cin>>m>>n;
+        G.resize(m,vector<bool>(n));
+        vis.resize(m,vector<bool>(n));
+        res=0;
+        land=0;
+        REP(i,m)
+        {
+            REP(j,n)
+            {
+                bool cell;
+                cin>>cell;
+                G[i][j]=cell;
+                vis[i][j]=false;
+            }
+        }
+        REP(i,m)
+        {
+            REP(j,n)
+            {
+                if(G[i][j] && !vis[i][j])
+                {
+                    connectedComponents(i,j);
+                    if(land)
+                    {
+                        res++;
+                    }
+                }
+                land=0;
+            }
+        }
+        cout<<res<<endl;
+    }
+    Bye;
+}
+
+/*
+2
+4 5
+1 1 1 1 0
+1 1 0 1 0
+1 1 0 0 0
+0 0 0 0 0
+4 5
+1 1 0 0 0
+1 1 0 0 0
+0 0 1 0 0
+0 0 0 1 1
+*/
+
+
+
