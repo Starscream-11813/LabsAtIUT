c0bcd50836f3a93e87e90d0f2da995c7ad605421
Syed Rifat Raiyan <46789391+Starscream-11813@users.noreply.github.com>
2023-05-22 18:39:18 +0600

    Create LabFinalTask4.cpp

---
+++ CSE 4810: Algorithm Engineering Lab/LabFinalTask4.cpp
@@ -0,0 +1,169 @@
+#include<bits/stdc++.h>
+using namespace std;
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set             tree<long long int, null_type, less<long long int>, rb_tree_tag, tree_order_statistics_node_update>
+template<class T> using min_heap=priority_queue<T,vector<T>,greater<T> >;
+#define LCM(a,b)                (a / __gcd(a,b) ) *b
+#define GCD(a,b)                __gcd(a,b)
+#define dist(x1,y1,x2,y2)       sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1))
+#define distl(x1,y1,x2,y2)      sqrtl((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1))
+#define SQR(a)                  ((a)*(a))
+#define fRead(x)                freopen(x,"r",stdin)
+#define fWrite(x)               freopen (x,"w",stdout)
+#define pb                      push_back
+#define mk                      make_pair
+#define ff                      first
+#define ss                      second
+#define lb                      lower_bound
+#define ub                      upper_bound
+#define all(a)                  a.begin(),a.end()
+#define lla(A)                  A.rbegin(), A.rend()
+#define Unique(a)               sort(all(a)),a.erase(unique(all(a)),a.end())
+#define lastEle(v)              v[v.size()-1]
+#define ABS(x)                  ((x)<0?-(x):(x))
+#define min3(a,b,c)             min(a,min(b,c))
+#define min4(a,b,c,d)           min(a,min(b,min(c,d)))
+#define max3(a,b,c)             max(a,max(b,c))
+#define max4(a,b,c,d)           max(a,max(b,max(c,d)))
+#define max5(a,b,c,d,e)         max(max3(a,b,c),max(d,e))
+#define min5(a,b,c,d,e)         min(min3(a,b,c),min(d,e))
+#define MEM(a,x)                memset(a,x,sizeof(a))
+#define Godspeed                ios_base::sync_with_stdio(0);cin.tie(NULL)
+#define urs(r...)               typename decay<decltype(r)>::type
+#define FOR(i,a,b)              for(urs(b) i=a;i<=b;i++)
+#define ROF(i,a,b)              for(urs(b) i=a;i>=b;i--)
+#define REP(i,b)                for(urs(b) i=0;i<b;i++)
+#define rep(i, begin, end)      for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define PER(i,a,b)              for(urs(b) i=a;i>b;i--)
+#define FOREACH(i,t)            for (__typeof(t.begin()) i=t.begin(); i!=t.end(); i++)
+#define PRESENT(c,x)            ((c).find(x) != (c).end())
+#define CPRESENT(c,x)           (find(all(c),x) != (c).end())
+#define SPRESENT(c,x)           ((c).find(x) != string::npos)
+#define Lower_bound(v, x)       distance(v.begin(), lower_bound(v.begin(), v.end(), x))
+#define Upper_bound(v, x)       distance(v.begin(), upper_bound(v.begin(), v.end(), x))
+#define Bye                     return 0
+#define SORT(v)                 sort(v.begin(),v.end())
+#define revsort(a,n)            sort(a,a+n); reverse(a,a+n)
+#define ODD(x)                  (bool)(x&1)
+#define Debug                   cout<<endl<<"I AM BATMAN"<<endl;
+#define what_is(x)              cerr << __LINE__ <<": " << #x << " is " << x << endl;
+#define bin_least_sig_onebit(x) __builtin_ffs(x)
+#define bin_leading_zeros(x)    __builtin_clz(x)
+#define bin_trailing_zeros(x)   __builtin_ctz(x)
+#define bin_total_ones(x)       __builtin_popcount(x)
+#define getbit(n,i)             (((n)&(1<<(i)))!=0)
+#define setbit0(n,i)            ((n)&(~(1<<(i))))
+#define setbit1(n,i)            ((n)|(1<<(i)))
+#define togglebit(n,i)          ((n)^(1<<(i)))
+#define TIME                    (chrono::steady_clock::now().time_since_epoch().count())
+//Datatypes
+#define ll                      long long
+#define ULL                     unsigned long long
+#define LD                      long double
+#define UI                      unsigned int
+#define US                      unsigned short
+#define endl                    '\n'
+const long long int MOD = 1000000007; // 998244353
+const ll INF = 1e18;
+const ll inf = 0xFFFFFFFFFFFFFFFL;
+#define PI                      acos(-1.0)
+
+bool sortbysec(const pair<long long int,long long int> &a, const pair<long long int,long long int> &b)
+{
+    return (a.second < b.second);
+}
+
+//Debugging stuff
+#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+void err(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args)
+{
+    cerr << *it << " = " << a << endl;
+    err(++it, args...);
+}
+#define pii pair<int,int>
+#define pll pair<long long,long long>
+#define pdd pair<double,double>
+#define pldld pair<long long double, long long double>
+// find_by_order(k)  returns iterator to kth element starting from 0;
+// order_of_key(k) returns count of elements strictly smaller than k;
+/*
+    Syed Rifat Raiyan
+    IUT CSE-18
+    Beautifully struggle every day~
+*/
+vector<ll>adj[100001];
+vector<bool>vis(100001,false);
+stack<ll>s;
+void topSort(ll node)
+{
+    if(!vis[node])
+    {
+        vis[node]=true;
+        for(auto x:adj[node])
+        {
+            topSort(x);
+        }
+        s.push(node);
+    }
+}
+int main()
+{
+    Godspeed;
+    int Tests=1;
+    cin>>Tests;
+    while(Tests--)
+    {
+        ll n,m;
+        cin>>n>>m;
+        while(m--)
+        {
+            ll u,v;
+            cin>>u>>v;
+            adj[u].pb(v);
+            //adj[v].pb(u);
+        }
+        FOR(i,1,n)
+        {
+            if(!vis[i])
+            {
+                topSort(i);
+            }
+        }
+        while(!s.empty())
+        {
+            cout<<s.top()<<" ";
+            s.pop();
+        }
+        cout<<endl;
+        REP(i,n+1)
+        {
+            adj[i].clear();
+        }
+        fill(vis.begin(),vis.begin()+n+1,false);
+    }
+    Bye;
+}
+/*
+2
+6 8
+1 2
+1 3
+2 4
+3 4
+2 5
+3 6
+4 5
+4 6
+6 6
+6 3
+6 1
+5 1
+5 2
+3 4
+4 2
+*/
+
